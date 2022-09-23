0e4a41074b812d56eac2c50899684b75a3ad0fc3
Stalk Spectrum <stalkspectrum@gmail.com>
2022-09-24 00:51:33 +0800

    Workday fix1 24-09-2022

--- 01-17-PYTHON-14/codeboards.py
+++ 01-17-PYTHON-14/codeboards.py
@@ -51,3 +51,32 @@ values_dict = {
     'four': df['four'].mode()[0]
 }
 df.fillna(value=values_dict, inplace=True)
+
+
+'''
+Задание 6.1
+Надо немного модифицировать функцию outliers_iqr().
+
+def outliers_iqr(data, feature):
+    x = data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75)
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * 1.5)
+    upper_bound = quartile_3 + (iqr * 1.5)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
+
+Добавить в неё параметры left и right, которые задают число IQR влево и вправо
+от границ ящика (пусть по умолчанию они равны 1.5). Функция, как и раньше,
+должна возвращать потенциальные выбросы и очищенный DataFrame.
+'''
+def outliers_iqr_mod(data, feature, left=1.5, right=1.5):
+    x = data[feature]
+    quartile_1, quartile_3 = x.quantile(0.25), x.quantile(0.75)
+    iqr = quartile_3 - quartile_1
+    lower_bound = quartile_1 - (iqr * left)
+    upper_bound = quartile_3 + (iqr * right)
+    outliers = data[(x < lower_bound) | (x > upper_bound)]
+    cleaned = data[(x > lower_bound) & (x < upper_bound)]
+    return outliers, cleaned
