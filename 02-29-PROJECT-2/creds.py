014008c8dff32a9bc2847624cb651da91573bfb0
Stalk Spectrum <stalkspectrum@gmail.com>
2023-01-10 21:41:37 +0800

    Workday save3 10-01-2023

---
+++ 02-29-PROJECT-2/creds.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+##### DBNAME = 'skillfactory'
+DBNAME = 'project_sql'
+USER = 'skillfactory'
+PASSWORD = 'cCkxxLVrDE8EbvjueeMedPKt'
+HOST = '84.201.134.129'
+PORT = 5432
+
+#####=====----- THE END -----=====#########################################
